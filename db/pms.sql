0f5038a8b4958cd07dabe8649f779eb3b986b209
git <git@lotut.com>
2016-04-21 20:07:20 +0800

    费用表添加已支付状态

--- db/pms.sql
+++ db/pms.sql
@@ -326,7 +326,8 @@ VALUES
 INSERT INTO fee_payment_status (fee_payment_status_id, fee_payment_status_desc)
 VALUES
 	(1, '未缴'),
-	(2, '已缴');
+	(2, '已支付'),
+	(3, '已缴');
 	
 INSERT INTO notice_paper_apply_types (notice_paper_apply_type_id, notice_paper_apply_type_desc)
 VALUES
