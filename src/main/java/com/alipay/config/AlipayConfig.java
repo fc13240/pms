868cd054a0a9a255a6c23d220f18d114d1a95976
git <git@lotut.com>
2016-05-04 15:49:18 +0800

    alipay config

--- src/main/java/com/alipay/config/AlipayConfig.java
+++ src/main/java/com/alipay/config/AlipayConfig.java
@@ -25,10 +25,10 @@ public class AlipayConfig {
     public static String key = "k4rr3am0jenqkm4im6kx1d4r33595ylt";
 
 	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String notify_url = "http://117.40.196.167:8080/pms/alipay/notify.html";
+	public static String notify_url = "http://g.lotut.com/alipay/notify.html";
 
 	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String return_url = "http://127.0.0.1:8080/pms/alipay/return.html";
+	public static String return_url = "http://g.lotut.com/alipay/return.html";
 
 	// 签名方式
 	public static String sign_type = "MD5";
