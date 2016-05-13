94b15987abc180d1cd905a4d523437344aaa72b1
CaiWei <15256085730@sina.cn>
2016-05-13 10:10:31 +0800

    取消自动修改龙图腾公司发票抬头

--- src/main/java/com/lotut/pms/web/controller/OrderController.java
+++ src/main/java/com/lotut/pms/web/controller/OrderController.java
@@ -72,9 +72,6 @@ public class OrderController {
 		
 		List<Fee> fees = feeService.getFeesByIds(Arrays.asList(feeIds));
 		
-		if(order.getPostAddress()==null || order.getPostAddress().getId()==0){
-			feeService.changeFeesInvoiceTitle(Arrays.asList(feeIds),"龙图腾信息技术有限公司");
-		}
 		orderService.createOrder(order, fees,express,nationalInvoice,companyInvoice);
 		
 		model.addAttribute("orderId", order.getId());
