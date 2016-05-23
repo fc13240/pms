7f9f23f981d1e7df1c5f0f99e39a3eee561ce04e
qz <306203478@qq.com>
2016-05-23 15:23:15 +0800

    k

--- src/main/java/com/lotut/pms/web/util/WebUtils.java
+++ src/main/java/com/lotut/pms/web/util/WebUtils.java
@@ -20,9 +20,7 @@ public class WebUtils {
 	
 	public static int getPageSize(HttpSession session) {
 		final int DEFAULT_PAGE_SIZE = 10;
-		System.out.println("-----1----");
 		if (session.getAttribute("pageSize") == null) {
-			System.out.println("-----2-----");
 			System.out.println(session.getAttribute("pageSize")+"-ppp");
 			return DEFAULT_PAGE_SIZE;
 		}
