5e5db88632db25992ad50b7c33a5c3d0f9c404df
qz <306203478@qq.com>
2016-07-22 17:20:59 +0800

    aa

---
+++ src/main/java/com/lotut/pms/dao/mapper/PatentDocumentTemplateMapper.java
@@ -0,0 +1,7 @@
+package com.lotut.pms.dao.mapper;
+
+import com.lotut.pms.domain.PatentDocumentTemplate;
+
+public interface PatentDocumentTemplateMapper {
+	void savePatentDocumentTemplate(PatentDocumentTemplate patentDocumentTemplate);
+}
---
+++ src/main/java/com/lotut/pms/dao/mybatis/PatentDocumentTemplateMybatisDao.java
@@ -0,0 +1,20 @@
+package com.lotut.pms.dao.mybatis;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import com.lotut.pms.dao.PatentDocumentTemplateDao;
+import com.lotut.pms.dao.mapper.PatentDocumentTemplateMapper;
+import com.lotut.pms.domain.PatentDocumentTemplate;
+
+public class PatentDocumentTemplateMybatisDao extends SqlSessionDaoSupport implements PatentDocumentTemplateDao{
+	private PatentDocumentTemplateMapper patentDocumentTemplateMapper;
+	
+	public void setPatentDocumentTemplateMapper(PatentDocumentTemplateMapper patentDocumentTemplateMapper){
+		this.patentDocumentTemplateMapper = patentDocumentTemplateMapper;
+	}
+	@Override
+	public void savePatentDocumentTemplate(PatentDocumentTemplate patentDocumentTemplate) {
+		patentDocumentTemplateMapper.savePatentDocumentTemplate(patentDocumentTemplate);
+	}
+
+}
