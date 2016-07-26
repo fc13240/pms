7ca95fe440e1ca36df20efdd0fea88b30e1583f8
qz <306203478@qq.com>
2016-07-26 08:40:35 +0800

    update error

--- src/main/java/com/lotut/pms/web/patentWriteController/PatentWriterController1.java
+++ src/main/java/com/lotut/pms/web/patentWriteController/PatentWriterController1.java
@@ -1,5 +1,6 @@
 package com.lotut.pms.web.patentWriteController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class PatentWriterController1 {
 	
 	private PatentDocumentTemplateService patentDocumentTemplateService;
 	
+	@Autowired
 	public PatentWriterController1(PatentDocumentTemplateService patentDocumentTemplateService) {
 		super();
 		this.patentDocumentTemplateService = patentDocumentTemplateService;
