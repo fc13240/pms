308a214522685e575f1cc2fb8dc732662e1f5723
CaiWei <15256085730@sina.cn>
2016-08-01 15:27:16 +0800

    a

--- src/main/java/com/lotut/pms/web/patentWriteController/UploadController.java
+++ src/main/java/com/lotut/pms/web/patentWriteController/UploadController.java
@@ -135,66 +135,67 @@ public class UploadController {
 	   
 	   
 	   @RequestMapping(path = "/uploadPic",method=RequestMethod.POST)
-		public void uploadPic(HttpServletRequest request1 ,HttpServletResponse response1) throws IOException {
-		   
-		   String savePath = Settings.PATENTDOC_ATTACHMENT_PATH;
-			
-			String path = request1.getContextPath();
-			String basePath = request1.getScheme() + "://" + request1.getServerName() + ":" + request1.getServerPort()
-			+ path;
-			String saveUrl = basePath + "/patentDocAttachment/";
-			String dirName = request1.getParameter("dir");
-			if (dirName == null) {
-				dirName = "image";
-			}
-			// 创建文件夹
-			savePath += dirName + "/";
-			saveUrl += dirName + "/";
-			File saveDirFile = new File(savePath);
-			if (!saveDirFile.exists()) {
-				saveDirFile.mkdirs();
-			}
-		   CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request1.getSession().getServletContext());
-		   if(multipartResolver.isMultipart(request1)){ 
-			 //转换成多部分request      
-	            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request1;  
-	            // 取得request中的所有文件名  
-	            Iterator<String> iter = multiRequest.getFileNames();  
-	            while (iter.hasNext()) {  
-	                // 取得上传文件  
-	                MultipartFile file = multiRequest.getFile(iter.next());  
-	                // 数据封装操作 MultipartFile reqeust  
-	                // 取得当前上传文件的文 件名称  
-	                String fileName = file.getOriginalFilename(); 
-	                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-	        		String ymd = sdf.format(new Date());
-	        		savePath += ymd + "/";
-	        		saveUrl += ymd + "/";
-	        		File dirFile = new File(savePath);
-	        		if (!dirFile.exists()) {
-	        			dirFile.mkdirs();
-	        		}
-	        		String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-	        		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-	        		String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
-	        		InputStream is = file.getInputStream();
-	        		int BUFFER_SIZE = 8 * 1024;
-	        		byte[] buffer = new byte[BUFFER_SIZE];
-	        		try (OutputStream out = new FileOutputStream(savePath + newFileName);) {
-	        			int bytesRead = -1;
-	        			while ((bytesRead = is.read(buffer)) != -1) {
-	        				out.write(buffer, 0, bytesRead);
-	        			}
-	        			out.flush();
-	        		}
-	        		String url=saveUrl+newFileName+".html";
-	       		 	Map<String, Object> succMap = new HashMap<String, Object>(); 
-	                succMap.put("url", saveUrl + newFileName+".html");  
-	                WebUtils.writeJsonStrToResponse(response1, succMap);
+		public void uploadPic(HttpServletRequest request1 ,HttpServletResponse response1)  {
+		   try{
+			   String savePath = Settings.PATENTDOC_ATTACHMENT_PATH;
+				
+				String path = request1.getContextPath();
+				String basePath = request1.getScheme() + "://" + request1.getServerName() + ":" + request1.getServerPort()
+				+ path;
+				String saveUrl = basePath + "/patentDocAttachment/";
+				String dirName = request1.getParameter("dir");
+				if (dirName == null) {
+					dirName = "image";
+				}
+				// 创建文件夹
+				savePath += dirName + "/";
+				saveUrl += dirName + "/";
+				File saveDirFile = new File(savePath);
+				if (!saveDirFile.exists()) {
+					saveDirFile.mkdirs();
+				}
+			   CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request1.getSession().getServletContext());
+			   if(multipartResolver.isMultipart(request1)){ 
+				 //转换成多部分request      
+		            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request1;  
+		            // 取得request中的所有文件名  
+		            Iterator<String> iter = multiRequest.getFileNames();  
+		            while (iter.hasNext()) {  
+		                // 取得上传文件  
+		                MultipartFile file = multiRequest.getFile(iter.next());  
+		                // 数据封装操作 MultipartFile reqeust  
+		                // 取得当前上传文件的文 件名称  
+		                String fileName = file.getOriginalFilename(); 
+		                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		        		String ymd = sdf.format(new Date());
+		        		savePath += ymd + "/";
+		        		saveUrl += ymd + "/";
+		        		File dirFile = new File(savePath);
+		        		if (!dirFile.exists()) {
+		        			dirFile.mkdirs();
+		        		}
+		        		String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+		        		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+		        		String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
+		        		InputStream is = file.getInputStream();
+		        		int BUFFER_SIZE = 8 * 1024;
+		        		byte[] buffer = new byte[BUFFER_SIZE];
+		        		try (OutputStream out = new FileOutputStream(savePath + newFileName);) {
+		        			int bytesRead = -1;
+		        			while ((bytesRead = is.read(buffer)) != -1) {
+		        				out.write(buffer, 0, bytesRead);
+		        			}
+		        			out.flush();
+		        		}
+		        		String url=saveUrl+newFileName+".html";
+		       		 	Map<String, Object> succMap = new HashMap<String, Object>(); 
+		                succMap.put("url", saveUrl + newFileName+".html");  
+		                WebUtils.writeJsonStrToResponse(response1, succMap);
+			   }
 		   }
-		   
-		    
-	   
-	   }
+		   }catch(IOException e){
+			   e.printStackTrace();
+		   }
+
 	   }
 }
