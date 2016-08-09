e4f983860babeaaaf41f1205ccf551f7d2982eba
CaiWei <15256085730@sina.cn>
2016-08-09 09:27:48 +0800

    a

--- src/main/java/com/lotut/pms/web/util/ParseHtmlToWord.java
+++ src/main/java/com/lotut/pms/web/util/ParseHtmlToWord.java
@@ -8,17 +8,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.log4j.chainsaw.Main;
-import org.apache.poi.hssf.record.Record;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.usermodel.Range;
-import org.apache.poi.poifs.filesystem.DirectoryEntry;
-import org.apache.poi.poifs.filesystem.DocumentEntry;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
 import org.junit.Test;
 
 public class ParseHtmlToWord {
@@ -31,10 +23,10 @@ public class ParseHtmlToWord {
 		      Range range = doc.getRange();  
 		      //把range范围内的${reportDate}替换为当前的日期  
 		      range.replaceText("${reportDate}", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));  
-		      range.replaceText("${appleAmt}", "100.00");  
+		      range.replaceText("${appleAmt}", "<div>测试</div>");  
 		      range.replaceText("${bananaAmt}", "200.00");  
 		      range.replaceText("${totalAmt}", "300.00");  
-		      OutputStream os = new FileOutputStream("D:\\word\\write.doc");  
+		      OutputStream os = new FileOutputStream("D:\\test.doc");  
 		      //把doc输出到输出流中  
 		      doc.write(os);  
 		      this.closeStream(os);  
