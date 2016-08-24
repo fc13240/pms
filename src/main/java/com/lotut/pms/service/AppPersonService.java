e1aec0a25f20a42c71670026d89dc5b7d229622d
xw <109773066@qq.com>
2016-08-24 14:41:34 +0800

    a

--- src/main/java/com/lotut/pms/service/AppPersonService.java
+++ src/main/java/com/lotut/pms/service/AppPersonService.java
@@ -6,18 +6,34 @@ import com.lotut.pms.domain.UserAppPerson;
 
 public interface AppPersonService {
 	void addAppPerson(CommonAppPerson commonAppPerson);
+
 	List<CommonAppPerson> getAllAppPersonByUser(int userId);
+
 	CommonAppPerson getOneAppPersonById(int id);
+
 	void updateAppPerson(CommonAppPerson AppPerson);
+
 	void deleteAppPersonById(int id);
+
 	List<CommonAppPerson> getUserAppPersons(int userId);
+
 	void saveAttachmentFile(CommonAppPerson AppPerson);
+
 	String getAppPersonUrlById(int id);
+
 	void addCommonAppPerson(CommonAppPerson CommonAppPerson);
+
 	void saveProxyFile(CommonAppPerson AppPerson);
+
 	String getProxyUrlById(int id);
+
 	void deleteAttachmentFileById(int id);
+
 	void addUserAppPerson(UserAppPerson userAppPerson);
+
 	Integer getIdbyAppPerson(CommonAppPerson commonAppPerson);
+
 	void deleteUserAppPersonbyId(UserAppPerson userAppPerson);
+
+	void updateAppPersonUserIdNull(CommonAppPerson commonAppPerson);
 }
