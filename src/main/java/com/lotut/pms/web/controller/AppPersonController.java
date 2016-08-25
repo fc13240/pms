69b5ee9899706ecec3259645bea98b5c4dee25bb
xw <109773066@qq.com>
2016-08-25 09:20:37 +0800

    a

--- src/main/java/com/lotut/pms/web/controller/AppPersonController.java
+++ src/main/java/com/lotut/pms/web/controller/AppPersonController.java
@@ -348,13 +348,14 @@ public class AppPersonController {
 		response.setContentType("application/doc");
 		String relativeUrl="linus常用命令整理";
 		String filePath=Settings.PROXY_TEMPLATE_FILE_PATH+relativeUrl;
+		System.out.println(filePath);
 		File appPersonFile = new File(filePath);
-		String downloadFileName = URLEncoder.encode(relativeUrl.substring(relativeUrl.lastIndexOf("/")+1), "UTF8");
+		String downloadFileName = URLEncoder.encode(relativeUrl, "UTF8");
 		if("FF".equals(getBrowser(request))){
 		    //针对火狐浏览器处理
-			downloadFileName =new String(relativeUrl.substring(relativeUrl.lastIndexOf("/")+1).getBytes("UTF-8"),"iso-8859-1");
+			downloadFileName =new String(relativeUrl.getBytes("UTF-8"),"iso-8859-1");
 		}
-		response.setHeader("Content-Disposition", "proxy;filename=" + downloadFileName);
+		response.setHeader("Content-Disposition", "proxyTemplate;filename=" + downloadFileName);
 		response.setContentLength((int)appPersonFile.length());
 		WebUtils.writeStreamToResponse(response, new FileInputStream(appPersonFile));
 	}
