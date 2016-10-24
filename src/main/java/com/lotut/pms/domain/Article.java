42292cf7d4ab28e65339b7dd9ae229fc24ed3363
xw <109773066@qq.com>
2016-10-24 09:26:45 +0800

    a

--- src/main/java/com/lotut/pms/domain/Article.java
+++ src/main/java/com/lotut/pms/domain/Article.java
@@ -14,15 +14,107 @@ public class Article {
 	private String keywords;
 	private String author;
 	private String title;
-	private Timestamp create_time;
+	@DateTimeFormat(iso=ISO.DATE_TIME,pattern="yyyy-MM-dd HH:mm:ss")
+	private Date create_time;
 	@DateTimeFormat(iso=ISO.DATE_TIME,pattern="yyyy-MM-dd HH:mm:ss")
 	private Date publish_time;
-	@DateTimeFormat(iso=ISO.DATE_TIME,pattern="yyyy-MM-dd HH:mm:ss")
-	private Date update_time;
+	private Timestamp update_time;
 	private String source;
 	private String content;
 	private String articleAbstract;
 	private int checkStatus;
 	private int upVote;
 	private int downVote;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArticleType getArticleType() {
+		return articleType;
+	}
+	public void setArticleType(ArticleType articleType) {
+		this.articleType = articleType;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+	public Date getPublish_time() {
+		return publish_time;
+	}
+	public void setPublish_time(Date publish_time) {
+		this.publish_time = publish_time;
+	}
+	public Timestamp getUpdate_time() {
+		return update_time;
+	}
+	public void setUpdate_time(Timestamp update_time) {
+		this.update_time = update_time;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getArticleAbstract() {
+		return articleAbstract;
+	}
+	public void setArticleAbstract(String articleAbstract) {
+		this.articleAbstract = articleAbstract;
+	}
+	public int getCheckStatus() {
+		return checkStatus;
+	}
+	public void setCheckStatus(int checkStatus) {
+		this.checkStatus = checkStatus;
+	}
+	public int getUpVote() {
+		return upVote;
+	}
+	public void setUpVote(int upVote) {
+		this.upVote = upVote;
+	}
+	public int getDownVote() {
+		return downVote;
+	}
+	public void setDownVote(int downVote) {
+		this.downVote = downVote;
+	}
+	
+	
 }
