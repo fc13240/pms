13b812ead1c263d1531ab74cb9ce8ea2573c2656
CaiWei <15256085730@sina.cn>
2016-10-24 10:41:37 +0800

    a

--- src/main/java/com/lotut/pms/web/controller/NewsController.java
+++ src/main/java/com/lotut/pms/web/controller/NewsController.java
@@ -1,10 +1,18 @@
 package com.lotut.pms.web.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.lotut.pms.domain.Page;
 import com.lotut.pms.service.NewsService;
+
 @Controller
 @RequestMapping(path="/news")
 public class NewsController {
@@ -15,5 +23,10 @@ public class NewsController {
 		this.newsService = newsService;
 	}
 	
+	
+	@RequestMapping(path="/list", method=RequestMethod.GET)
+	public String getUserNewsByPage(Model model,Page page,HttpSession session) {
+		return "news_list";
+	}
 	
 }
