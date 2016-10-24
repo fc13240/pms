1f2f881316e4805465611770f7417fa010bd09ea
xw <109773066@qq.com>
2016-10-24 13:50:18 +0800

    列表

--- src/main/java/com/lotut/pms/dao/mybatis/ArticleMybatisDao.java
+++ src/main/java/com/lotut/pms/dao/mybatis/ArticleMybatisDao.java
@@ -1,16 +1,30 @@
 package com.lotut.pms.dao.mybatis;
 
 
+import java.util.List;
+
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 import com.lotut.pms.dao.ArticleDao;
 import com.lotut.pms.dao.mapper.ArticleMapper;
+import com.lotut.pms.domain.Article;
+import com.lotut.pms.domain.Page;
 
 public class ArticleMybatisDao extends SqlSessionDaoSupport implements ArticleDao{
 	private ArticleMapper articleMapper;
 
 	public void setArticleMapper(ArticleMapper articleMapper) {
 		this.articleMapper = articleMapper;
+	}
+
+	@Override
+	public List<Article> getUserArticleByPage(Page page) {
+		return articleMapper.getUserArticleByPage(page);
+	}
+
+	@Override
+	public int getUserArticleCount(int userId) {
+		return articleMapper.getUserArticleCount(userId);
 	}
 	
 }
--- src/main/java/com/lotut/pms/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/lotut/pms/service/impl/ArticleServiceImpl.java
@@ -1,7 +1,19 @@
 package com.lotut.pms.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import com.lotut.pms.dao.ArticleDao;
+import com.lotut.pms.domain.Article;
+import com.lotut.pms.domain.CommonAppPerson;
+import com.lotut.pms.domain.Page;
+import com.lotut.pms.domain.UserArticle;
 import com.lotut.pms.service.ArticleService;
+import com.lotut.pms.util.PrincipalUtils;
+import com.lotut.pms.web.util.WebUtils;
 
 public class ArticleServiceImpl implements ArticleService {
 	private ArticleDao articleDao;
@@ -9,5 +21,23 @@ public class ArticleServiceImpl implements ArticleService {
 	public ArticleServiceImpl(ArticleDao articleDao) {
 		this.articleDao = articleDao;
 	}
+
+	@Override
+	public UserArticle getUserArticleByPage(Page page, HttpSession session) {
+		int userId = PrincipalUtils.getCurrentUserId();
+		page.setUserId(userId);
+		page.setPageSize(WebUtils.getPageSize(session));
+		if (page.getCurrentPage() <= 0) {
+			page.setCurrentPage(1);
+		}
+		int totalCount=articleDao.getUserArticleCount(userId);
+		page.setTotalRecords(totalCount);
+		List<Article> articles=articleDao.getUserArticleByPage(page);
+		UserArticle userArticle=new UserArticle();
+		userArticle.setPage(page);
+		userArticle.setArticles(articles);
+		return userArticle;
+	}
+
 	
 }
