2d7ada65a1d7b983277700999cd9489a1d658b82
CaiWei <15256085730@sina.cn>
2016-10-24 09:34:56 +0800

    a

---
+++ src/main/java/com/lotut/pms/web/controller/NewsController.java
@@ -0,0 +1,19 @@
+package com.lotut.pms.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lotut.pms.service.NewsService;
+@Controller
+@RequestMapping(path="/news")
+public class NewsController {
+	private NewsService newsService;
+	
+	@Autowired
+	public NewsController(NewsService newsService) {
+		this.newsService = newsService;
+	}
+	
+	
+}
