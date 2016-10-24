13ddf462ccd6709b80ad77e2d32d5c8a8e61013f
qz <306203478@qq.com>
2016-10-24 09:26:14 +0800

    修改配置文件

--- src/main/java/com/lotut/pms/domain/News.java
+++ src/main/java/com/lotut/pms/domain/News.java
@@ -21,4 +21,78 @@ public class News {
 	private String source;
 	private String content;
 	private String newsAbstract;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public NewsType getNewsType() {
+		return newsType;
+	}
+	public void setNewsType(NewsType newsType) {
+		this.newsType = newsType;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Timestamp getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(Timestamp create_time) {
+		this.create_time = create_time;
+	}
+	public Date getPublish_time() {
+		return publish_time;
+	}
+	public void setPublish_time(Date publish_time) {
+		this.publish_time = publish_time;
+	}
+	public Date getUpdate_time() {
+		return update_time;
+	}
+	public void setUpdate_time(Date update_time) {
+		this.update_time = update_time;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getNewsAbstract() {
+		return newsAbstract;
+	}
+	public void setNewsAbstract(String newsAbstract) {
+		this.newsAbstract = newsAbstract;
+	}
+	
+	
 }
