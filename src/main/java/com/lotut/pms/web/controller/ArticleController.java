7c870436dbc7917a9480d4817694392e2b9461c8
xw <109773066@qq.com>
2016-10-25 10:41:15 +0800

    a

--- src/main/java/com/lotut/pms/web/controller/ArticleController.java
+++ src/main/java/com/lotut/pms/web/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.lotut.pms.domain.Article;
 import com.lotut.pms.domain.ArticleSearchCondition;
 import com.lotut.pms.domain.ArticleType;
 import com.lotut.pms.domain.Page;
