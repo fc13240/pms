153b9b34eeeb31400f01f32fcbbf75b14c9285c9
xw <109773066@qq.com>
2016-12-06 11:32:25 +0800

    sell_second_column.sql

---
+++ db/sell_second_column.sql
@@ -0,0 +1,72 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.5.49 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `sell_second_column` (
+	`patent_second_column_id` int (11),
+	`patent_second_column_name` varchar (765),
+	`patent_first_column_id` int (11)
+); 
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('1','日常生活领域','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('2','服装/鞋帽','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('3','家具/家装','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('4','家电/照明','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('5','艺术/乐器','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('6','教学/学习','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('7','市政/环卫','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('8','体育/健身','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('9','行政/办公','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('10','娱乐/玩具','1');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('11','农林渔牧/食品','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('12','农业','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('13','林业','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('14','渔业','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('15','畜牧业','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('16','肥料','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('17','饲料','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('18','农药','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('19','兽药','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('20','食品/酒饮','2');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('21','医疗/保健/美容','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('22','医药/中西药','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('23','医药加工','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('24','医疗器械','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('25','医疗用品','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('26','消毒洁净','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('27','保健品','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('28','保健器械','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('29','美容护肤','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('30','日化领域','3');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('31','机电/能源/环保','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('32','机电工程','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('33','机械工程','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('34','模具铸造','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('35','生产加工','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('36','五金配件','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('37','电力工程','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('38','新能源','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('39','矿产开采','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('40','环保领域','4');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('41','化工/冶金/纺织','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('42','化工设备','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('43','化工材料','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('44','石化领域','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('45','生物工程','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('46','基因工程','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('47','冶金领域','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('48','纺织领域','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('49','皮革领域','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('50','造纸领域','5');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('51','建筑/交通/通讯','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('52','建筑领域','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('53','水利公路桥梁','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('54','交通领域','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('55','交通工具及配件','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('56','物流仓储','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('57','通信领域','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('58','互联网物联网','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('59','电子电路及元件','6');
+insert into `sell_second_column` (`patent_second_column_id`, `patent_second_column_name`, `patent_first_column_id`) values('60','手机电脑及配件','6');
