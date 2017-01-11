6d9564eff4e6e17c7ac7e5991d8b9733d3aa36ba
CaiWei <15256085730@sina.cn>
2017-01-11 15:05:27 +0800

    a

--- src/main/java/com/lotut/pms/web/controller/BrandNoticeController.java
+++ src/main/java/com/lotut/pms/web/controller/BrandNoticeController.java
@@ -32,6 +32,30 @@ import com.lotut.pms.service.BrandNoticeService;
 import com.lotut.pms.util.PrincipalUtils;
 import com.lotut.pms.web.util.WebUtils;
 
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG
+    @author	CaiWei
+*/
+
 @Controller
 @RequestMapping(path="/brandNotice")
 public class BrandNoticeController {
